908e13dd302c6d90c3f0827b79573e8b4c44c3e6
Sealie1456 <aman.khanduja55@gmail.com>
2025-11-12 22:38:25 +0530

    app

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ export default function App() {
       </div>
 
       {/* Global interactive starfield (follows pointer / touch) */}
-      <Starfield />
+      {/* <Starfield /> */}
 
       {/* Global shooting stars animation */}
       <ShootingStars />
